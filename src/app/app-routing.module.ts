a084f4033abe7436226438ad0ace70ef21b8e808
Žan Žerak <zan.zerak99@gmail.com>
2023-10-17 08:11:59 +0200

    started scafolding the project, addind devextreme, route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { WildCardComponent } from './wild-card/wild-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponentComponent,
+  },
+  {
+    path: '**',
+    component: WildCardComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FavoriteViewComponent } from './favorite-view/favorite-view.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { WildCardComponent } from './wild-card/wild-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FavoriteViewComponent,
+    HomeComponentComponent,
+    WildCardComponent
   ],
   imports: [
     BrowserModule,
